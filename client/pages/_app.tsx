6393dba6d67b970edcf8b8026a93e5a0acaffc0c
Mishulka <misailgromov@gmail.com>
2025-09-29 21:23:12 +0300

    -chore: tailwind configured

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { customTheme } from "../themes/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={customTheme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+  return (<>
+  <Component {...pageProps} />
+  </>
   );
 }
 
